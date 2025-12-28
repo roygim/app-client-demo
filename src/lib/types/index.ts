914bc30302066bc20f10cfbfc1138d623c9b28dc
Roei <roygim@gmail.com>
2025-12-28 11:14:33 +0200

    roeig

---
+++ src/lib/types/index.ts
@@ -0,0 +1,19 @@
+export enum ResponseError {
+    InValidRequest = "InvalidRequest",
+    NotFound = "NotFound",
+    Unauthorized = "Unauthorized",
+    InternalServerError = "InternalServerError"
+}
+
+export enum UserRole {
+    Admin = 1,
+    Regular = 2
+}
+
+export interface User {
+    id: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    role: UserRole;
+}
